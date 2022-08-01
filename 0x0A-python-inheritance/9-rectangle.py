f0f90caee4d311c7ecad78727defdda9930f5b73
Solomon <100710166+TettehSolomon@users.noreply.github.com>
2022-08-01 13:19:05 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#Gloria Echendustop what you are doing
 """Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
